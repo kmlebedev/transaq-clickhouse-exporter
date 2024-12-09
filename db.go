3e7bc7496bd13c135cac081e56083e94f4f7e222
Konstantin Lebedev <9497591+kmlebedev@users.noreply.github.com>
2024-12-09 23:03:02 +0200

    Quotes (#1)
    * draft quotes
    
    * up deps txmlconnector
    
    * up deps connect grpc
    
    * fix NewTCClient
    
    ---------
    
    Co-authored-by: whitefox <whitefox@mayflower.work>

--- db.go
+++ db.go
@@ -1,12 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"github.com/kmlebedev/txmlconnector/client/commands"
+	"time"
+)
+
 const (
 	EnvKeyLogLevel          = "LOG_LEVEL"
 	ExportCandleCount       = 0
+	asyncInsertWait         = false
+	tradeTimeLayout         = "02.01.2006 15:04:05"
+	dateLayout              = "02.01.2006" // DD.MM.YYYY
+	tableTimeLayout         = "2006-01-02 15:04:05"
 	ChCandlesInsertQuery    = "INSERT INTO transaq_candles"
 	ChSecuritiesInsertQuery = "INSERT INTO transaq_securities"
 	ChTradesInsertQuery     = "INSERT INTO transaq_trades VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
 	ChSecInfoInsertQuery    = "INSERT INTO transaq_securities_info VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+	ChQuotesInsert          = "INSERT INTO transaq_quotes VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
 	candlesDDL = `CREATE TABLE IF NOT EXISTS transaq_candles (
 		date   DateTime('Europe/Moscow'),
@@ -77,8 +88,87 @@ const (
 		isin String,
 		regnumber String,
 		buybackprice Float32,
-		buybackdate Float32,
+		buybackdate DateTime('Europe/Moscow'),
 		currencyid String
 	) ENGINE = ReplacingMergeTree()
 	ORDER BY (sec_code, market, regnumber, isin)`
+
+	quotesDDL = `CREATE TABLE IF NOT EXISTS transaq_quotes (
+        time 	 DateTime('Europe/Moscow'),
+		secid    UInt16,
+		board 	 LowCardinality(String),
+		sec_code LowCardinality(FixedString(16)),
+    	price    Float32,
+		source   LowCardinality(String),
+        yield    Int8,
+        buy      Int16,
+        Sell     Int16,
+    ) ENGINE = ReplacingMergeTree()
+	ORDER BY (sec_code, board, price, source)
+    `
 )
+
+func insertQuote(quote *commands.Quote, eventTime *time.Time) error {
+	return connect.AsyncInsert(ctx, ChQuotesInsert, asyncInsertWait,
+		fmt.Sprint(eventTime.Format(tableTimeLayout)),
+		quote.SecId,
+		quote.Board,
+		quote.SecCode,
+		quote.Price,
+		quote.Source,
+		quote.Yield,
+		quote.Buy,
+		quote.Sell,
+	)
+}
+func insertTrade(trade *commands.Trade) error {
+	tradeTime, _ := time.Parse(tradeTimeLayout, trade.Time)
+	return connect.AsyncInsert(ctx, ChTradesInsertQuery, asyncInsertWait,
+		fmt.Sprint(tradeTime.Format(tableTimeLayout)),
+		trade.SecId,
+		trade.SecCode,
+		trade.TradeNo,
+		trade.Board,
+		trade.Price,
+		trade.Quantity,
+		trade.BuySell,
+		trade.OpenInterest,
+		trade.Period)
+}
+
+func insertSecInfo(secInfo *commands.SecInfo) error {
+	matDate, _ := time.Parse(dateLayout, secInfo.MatDate)
+	couponDate, _ := time.Parse(dateLayout, secInfo.CouponDate)
+	buybackDate, _ := time.Parse(dateLayout, secInfo.BuybackDate)
+	return connect.AsyncInsert(ctx, ChSecInfoInsertQuery, asyncInsertWait,
+		secInfo.SecId,
+		secInfo.SecName,
+		secInfo.SecCode,
+		secInfo.Market,
+		secInfo.PName,
+		fmt.Sprint(matDate.Format(tableTimeLayout)),
+		secInfo.ClearingPrice,
+		secInfo.MinPrice,
+		secInfo.MaxPrice,
+		secInfo.BuyDeposit,
+		secInfo.SellDeposit,
+		secInfo.BgoC,
+		secInfo.BgoNc,
+		secInfo.BgoBuy,
+		secInfo.AccruedInt,
+		secInfo.CouponValue,
+		fmt.Sprint(couponDate.Format(tableTimeLayout)),
+		secInfo.CouponPeriod,
+		secInfo.CouponPeriod,
+		secInfo.FaceValue,
+		secInfo.PutCall,
+		secInfo.PointCost,
+		secInfo.OptType,
+		secInfo.LotVolume,
+		secInfo.Isin,
+		secInfo.RegNumber,
+		secInfo.BuybackPrice,
+		fmt.Sprint(buybackDate.Format(tableTimeLayout)),
+		secInfo.CurrencyId,
+	)
+}
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.23.3
 
 require (
 	github.com/ClickHouse/clickhouse-go/v2 v2.30.0
-	github.com/kmlebedev/txmlconnector v1.26.3-0.20241207125021-c20fafcc442c
+	github.com/kmlebedev/txmlconnector v1.26.6
 	github.com/sirupsen/logrus v1.9.3
 )
 
@@ -13,7 +13,6 @@ require (
 	github.com/andybalholm/brotli v1.1.1 // indirect
 	github.com/go-faster/city v1.0.1 // indirect
 	github.com/go-faster/errors v0.7.1 // indirect
-	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.7 // indirect
 	github.com/paulmach/orb v0.11.1 // indirect
@@ -27,7 +26,7 @@ require (
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241206012308-a4fef0638583 // indirect
-	google.golang.org/grpc v1.68.0 // indirect
+	google.golang.org/grpc v1.68.1 // indirect
 	google.golang.org/protobuf v1.35.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
